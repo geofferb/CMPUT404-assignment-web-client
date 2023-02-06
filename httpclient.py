07a399068896e83fea105b1b1c95fda3ec74c275
geofferb <38046772+geofferb@users.noreply.github.com>
2023-02-06 12:47:27 -0700

    start assignment

--- httpclient.py
+++ httpclient.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 # coding: utf-8
 # Copyright 2016 Abram Hindle, https://github.com/tywtyw2002, and https://github.com/treedust
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,19 +21,23 @@
 import sys
 import socket
 import re
+from email.utils import formatdate
 # you may use urllib to encode data appropriately
 import urllib.parse
 
+
 def help():
     print("httpclient.py [GET/POST] [URL]\n")
+
 
 class HTTPResponse(object):
     def __init__(self, code=200, body=""):
         self.code = code
         self.body = body
 
+
 class HTTPClient(object):
-    #def get_host_port(self,url):
+    # def get_host_port(self,url):
 
     def connect(self, host, port):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -43,15 +47,15 @@ class HTTPClient(object):
     def get_code(self, data):
         return None
 
-    def get_headers(self,data):
+    def get_headers(self, data):
         return None
 
     def get_body(self, data):
         return None
-    
+
     def sendall(self, data):
         self.socket.sendall(data.encode('utf-8'))
-        
+
     def close(self):
         self.socket.close()
 
@@ -67,9 +71,33 @@ class HTTPClient(object):
                 done = not part
         return buffer.decode('utf-8')
 
+    def sendRequest(self, url, hostname, body="", otherFields={}):
+        date = formatdate(usegmt=True)  # HTTP formatted data
+        header = f"GET {url} HTTP/1.1 \r\nHost: {hostname}\r\nDate: {date}\r\n"
+        if otherFields:
+            for field, value in otherFields.items():
+                header += f"{field}: {value}\r\n"
+
+        # if body is not in binary format, it needs to be converted
+        if not isinstance(body, (bytes, bytearray)):
+            body = bytearray(body, 'utf-8')
+
+        rq = bytearray(header + "\r\n", 'utf-8') + body
+        # print(rq)
+        self.sendall(rq)
+
     def GET(self, url, args=None):
         code = 500
         body = ""
+        u = urllib.parse.urlparse(url)
+        port = u.port if u.port else 8080
+        self.connect(u.hostname, port)
+        self.sendRequest(url, u.hostname)
+        self.socket.shutdown(socket.SHUT_WR)
+        response = self.recvall()
+        self.close()
+        print(response)
+
         return HTTPResponse(code, body)
 
     def POST(self, url, args=None):
@@ -79,10 +107,11 @@ class HTTPClient(object):
 
     def command(self, url, command="GET", args=None):
         if (command == "POST"):
-            return self.POST( url, args )
+            return self.POST(url, args)
         else:
-            return self.GET( url, args )
-    
+            return self.GET(url, args)
+
+
 if __name__ == "__main__":
     client = HTTPClient()
     command = "GET"
@@ -90,6 +119,6 @@ if __name__ == "__main__":
         help()
         sys.exit(1)
     elif (len(sys.argv) == 3):
-        print(client.command( sys.argv[2], sys.argv[1] ))
+        print(client.command(sys.argv[2], sys.argv[1]))
     else:
-        print(client.command( sys.argv[1] ))
+        print(client.command(sys.argv[1]))
